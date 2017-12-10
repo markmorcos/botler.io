16cd37be7c2ccbf475f27f4ac2efcfcf093c0f07
Mark Morcos <mark.yehia@gmail.com>
2017-12-10 22:35:47 +0200

    Modified server to use JWT

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView, DetailsView, UserView, UserDetailsView
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = {
     url(r'^messages/$', CreateView.as_view(), name="create"),
@@ -12,7 +12,7 @@ urlpatterns = {
     url(r'^users/$', UserView.as_view(), name="users"),
     url(r'users/(?P<pk>[0-9]+)/$',
         UserDetailsView.as_view(), name="user_details"),
-    url(r'^get-token/', obtain_auth_token),
+    url(r'^get-token/', obtain_jwt_token),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- server/server/settings.py
+++ server/server/settings.py
@@ -132,7 +132,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     )
 }
